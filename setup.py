437488039827f13e0a6d05b7c7740379cb13141b
Manuel Gil <manuelgilsitio@gmail.com>
2021-12-23 00:02:18 -0500

    project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="youtube_scraper",
+    version="0.1",
+    description="Useful tools to get information from youtube videos",
+    author ="Manuel Gil",
+    author_email="manuelgilsitio@gmail.com",
+    packages=find_packages()
+
+)
