8195162e5093aee407095f861278619b5d542bbc
Manuel Gil <manuelgilsitio@gmail.com>
2021-12-23 00:36:18 -0500

    example script has been added

---
+++ examples/retrieving_basic_info.py
@@ -0,0 +1,27 @@
+from youtube_scraper import Scraper
+import argparse
+import time
+
+parser = argparse.ArgumentParser(description="")
+parser.add_argument("--url", help= "Video url")
+
+
+def main(url):
+    if url is None:
+        url = "https://www.youtube.com/watch?v=9mtlSiKm3kg"
+    
+    my_scraper = Scraper(url)
+    time.sleep(15)
+    my_scraper.get_video_info()
+
+    print(f"Video Title: {my_scraper.title.text}")
+    print(f"Total Views: {my_scraper.count_views.text}")
+    print(f"Total Likes: {my_scraper.count_likes.text}")
+    print(f"Video Date: {my_scraper.date.text}")
+
+    print(f"Channel Name: {my_scraper.channel_name.text}")
+    print(f"Subscribers: {my_scraper.channel_subs.text}")
+
+if __name__ =="__main__":
+    args = parser.parse_args()
+    main(args.url)
--- youtube_scraper/scrapers/scrapers.py
+++ youtube_scraper/scrapers/scrapers.py
@@ -30,29 +30,10 @@ class Scraper:
         self.count_views = info_content.find_element_by_class_name(class_name_count_views)
         self.count_likes = info_content.find_element_by_class_name(class_name_like_button).\
             find_element_by_id("text")
-        self.date = info_content.find_element_by_id("text")
+        self.date = info_content.find_element_by_id("info-strings")
 
         self.channel_name = meta_content.find_element_by_id("upload-info").find_element_by_id("channel-name")
         self.channel_subs = meta_content.find_element_by_id("upload-info").find_element_by_id("owner-sub-count")
 
         self.channel_url = self.channel_name.find_element_by_class_name("yt-simple-endpoint.style-scope.yt-formatted-string")
         self.video_description = meta_content.find_element_by_id("description")
-
-
-
-# my_scraper = Scraper("https://www.youtube.com/watch?v=9mtlSiKm3kg")
-# time.sleep(15)
-# my_scraper.get_video_info()
-
-# print(my_scraper.title.text)
-# print(my_scraper.count_views.text)
-# print(my_scraper.count_likes.text)
-# print(my_scraper.date.text)
-
-# print(my_scraper.channel_name.text)
-# print(my_scraper.channel_url.get_attribute("href"))
-# print(my_scraper.channel_subs.text)
-# print(my_scraper.video_description.text)
-
-
-
